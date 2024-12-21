33fdc8814c8fd6ebf8a9b17fbca968204d912927
Vishwa <Vishwa@vishwa-pc>
2024-12-21 21:13:16 +0530

    first commit

---
+++ src/main/java/com/kodeville/eduxplore/EduxploreApplication.java
@@ -0,0 +1,13 @@
+package com.kodeville.eduxplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduxploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduxploreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodeville/eduxplore/EduxploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodeville.eduxplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduxploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
